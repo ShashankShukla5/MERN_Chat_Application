c5ee10c746c228e1e952e90b32f1af00c097a2eb
ShashankShukla5 <shashank9450158183@gmail.com>
2025-06-02 01:08:54 +0530

    Deployment Ready

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+import path from "path"
+
 import authRoutes from './src/routes/auth.route.js';
 import messageRoutes from './src/routes/message.route.js';
 
@@ -12,6 +14,7 @@ import { app, server } from './src/lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve;
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -24,6 +27,13 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
